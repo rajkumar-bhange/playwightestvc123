1f19a84b52fdd7c29d985dfcc66af7785d50e2d4
rajkumar-bhange <rajkumarbh45@gmail.com>
2025-07-28 20:07:16 +0530

    kk

--- tests/demo.spec.js
+++
@@ -1,5 +0,0 @@
-import {test,expect} from '@playwright/test'
-test("click the index value",async({page})=>{
-    await page.goto('https://practicetestautomation.com/')
-    await page.click("(//a[normalize-space()='Practice'])[1]")
-})
